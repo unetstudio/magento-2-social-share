78098b1bab59a557761378543812e2ac547ea16c
Duc Dao Huu - ducdh1 <ducdh1@smartosc.com>
2017-04-14 10:54:40 +0700

    Add Unit test

---
+++ Test/Unit/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Copyright © PART <info@part-online.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+// @codingStandardsIgnoreFile
+require_once realpath(__DIR__ . '/../../vendor/autoload.php');
+/**
+ * @SuppressWarnings(PHPMD.ShortMethodName)
+ */
+function __()
+{
+    return $argc = func_get_args();
+}
